841e7d69b71f4987763d9d617b321f4457b37ea6
bruce994 <26wz.wang@gmail.com>
2018-02-01 09:59:17 +0800

    wang

--- vote/views.py
+++ vote/views.py
@@ -78,7 +78,7 @@ def index(request):
     ad = Ad.objects.filter(vid = vid).order_by('-pub_date')
     ad_json  = serializers.serialize('json',ad)
 
-    data = '{"auditd_check":0,"misc":'+json.dumps(data1)+',"ad":'+ad_json+',"vote":'+vote_json[1:-1]+',"list":'+latest_list_json+'}'
+    data = '{"auditd_check":1,"misc":'+json.dumps(data1)+',"ad":'+ad_json+',"vote":'+vote_json[1:-1]+',"list":'+latest_list_json+'}'
     return HttpResponse(data, content_type='json')
 
 
